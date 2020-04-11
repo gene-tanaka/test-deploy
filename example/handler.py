8ad5f02f7e844e2f204df2a6c46682766ce67a45
Anders Hafreager <anders.hafreager@cognite.com>
2020-04-12 01:26:36 +0200

    Testing shit

--- example/handler.py
+++ example/handler.py
@@ -2,6 +2,7 @@ def handle(data, client, secrets):
     print("Doing this!")
     assets = client.assets.list()
     print(f"Found {len(assets)} assets.")
+    
     return {
         "success": True
     }
